d8d26da56ddcf5ea05d378e7735538141a2a0a8b
tomernesimyan <49777140+tomernesimyan@users.noreply.github.com>
2020-03-30 21:48:27 +0300

    fixed all problems

--- app/src/main/java/com/example/oweme/PopUp.java
+++ app/src/main/java/com/example/oweme/PopUp.java
@@ -2,6 +2,9 @@ package com.example.oweme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
 public class PopUp extends Activity {
 
@@ -9,5 +12,16 @@ public class PopUp extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width* .6), (int)(height*.6));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+        getWindow().setAttributes(params);
     }
 }
